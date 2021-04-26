120c0c7cf98e8d92d0ca4e6c5dcdc8760684aa88
Deven Blake <blakedevendesu@gmail.com>
2021-04-25 21:42:59 -0400

    REAL zelda

---
+++ zeldb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+die() { return 1 || exit 1 }
+
+if ! which curl >/dev/null 2>&1; then
+	printf "1n574ll curl, 5700p1d!\n"
+	die
+fi
+
+ZELDA="http://www.trinity.moe/zelda.wav"
+
+if which aplay >/dev/null 2>&1; then
+	curl "$ZELDA" | aplay
+elif ls /dev/dsp >/dev/null 2>&1; then
+	curl "$ZELDA" >/dev/dsp
+else
+	printf "Unknown sound device. Sorry!\n"
+	die
+fi
+
+return 0 || exit 0
