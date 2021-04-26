e95d90e6ba8e0230a89588c083cbc6e255d3aa2e
Deven Blake <blakedevendesu@gmail.com>
2021-04-25 21:48:00 -0400

    fix syntax

--- zeldb.sh
+++ zeldb.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -x
 
-die() { return 1 || exit 1 }
+die() { return 1 || exit 1; }
 
 if ! which curl >/dev/null 2>&1; then
 	printf "1n574ll curl, 5700p1d!\n"
@@ -9,10 +9,8 @@ fi
 
 ZELDA="http://www.trinity.moe/zelda.wav"
 
-if which aplay >/dev/null 2>&1; then
-	curl "$ZELDA" | aplay
-elif ls /dev/dsp >/dev/null 2>&1; then
-	curl "$ZELDA" >/dev/dsp
+if   which aplay >/dev/null 2>&1; then curl "$ZELDA" | aplay
+elif ls /dev/dsp >/dev/null 2>&1; then curl "$ZELDA" >/dev/dsp
 else
 	printf "Unknown sound device. Sorry!\n"
 	die
