4aaed14898a36860dd5cc34744221f88dfe85c96
Deven Blake <blakedevendesu@gmail.com>
2021-05-10 08:51:22 -0400

    dying considered scary

--- zeldb.sh
+++ zeldb.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -x
-
-die() { return 1 || exit 1; }
+set -e
 
 if ! which curl >/dev/null 2>&1; then
 	printf "1n574ll curl, 5700p1d!\n"
-	die
+	exit 1
 fi
 
 ZELDA="http://www.trinity.moe/zelda.wav"
@@ -13,7 +12,7 @@ if   which aplay >/dev/null 2>&1; then curl "$ZELDA" | aplay
 elif ls /dev/dsp >/dev/null 2>&1; then curl "$ZELDA" >/dev/dsp
 else
 	printf "Unknown sound device. Sorry!\n"
-	die
+	exit 1
 fi
 
-return 0 || exit 0
+exit 0
